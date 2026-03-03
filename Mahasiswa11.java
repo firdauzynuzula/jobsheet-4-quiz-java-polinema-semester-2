f22e0b3ea48ba5aea066d97a07989bd97560a008
firdauzynuzula <nuzulafirdhauzy@gmail.com>
2026-03-03 19:32:00 +0700

    mengubah isi file MahasiswaMain11.java dan Mahasiswa11.java

--- Mahasiswa11.java
+++ Mahasiswa11.java
@@ -1,3 +1,53 @@
 public class Mahasiswa11 {
-    
+    public String nim;
+    public String nama;
+    public int tahunMasuk;
+    public MataKuliah11[] mataKuliahDiambil;
+    private int jumlahMataKuliah;
+
+    // Konstruktor
+    public Mahasiswa11(String nim, String nama, int tahunMasuk) {
+        this.nim = nim;
+        this.nama = nama;
+        this.tahunMasuk = tahunMasuk;
+        this.mataKuliahDiambil = new MataKuliah11[10]; // kapasitas maksimal 10
+        this.jumlahMataKuliah = 0;
+    }
+
+    // Menampilkan informasi mahasiswa
+    public void tampilkanInfo() {
+        System.out.println("NIM         : " + nim);
+        System.out.println("Nama        : " + nama);
+        System.out.println("Tahun Masuk : " + tahunMasuk);
+        System.out.println("Total SKS   : " + hitungTotalSKS());
+        System.out.println("Mata Kuliah Diambil:");
+        if (jumlahMataKuliah == 0) {
+            System.out.println("  (belum ada mata kuliah)");
+        } else {
+            for (int i = 0; i < jumlahMataKuliah; i++) {
+                MataKuliah11 mk = mataKuliahDiambil[i];
+                System.out.println("  - " + mk.namaMatakuliah + " (" + mk.kodeMatakuliah + ") - " + mk.sks + " SKS");
+            }
+        }
+    }
+
+    // Menambahkan mata kuliah yang diambil
+    public void tambahMataKuliah(MataKuliah11 mataKuliah) {
+        if (jumlahMataKuliah >= mataKuliahDiambil.length) {
+            System.out.println("Gagal: kapasitas mata kuliah sudah penuh.");
+            return;
+        }
+        mataKuliahDiambil[jumlahMataKuliah] = mataKuliah;
+        jumlahMataKuliah++;
+        System.out.println("Mata kuliah '" + mataKuliah.namaMatakuliah + "' berhasil ditambahkan.");
+    }
+
+    // Menghitung total SKS yang diambil
+    public int hitungTotalSKS() {
+        int total = 0;
+        for (int i = 0; i < jumlahMataKuliah; i++) {
+            total += mataKuliahDiambil[i].sks;
+        }
+        return total;
+    }
 }
--- MahasiswaMain11.java
+++ MahasiswaMain11.java
@@ -1,3 +1,34 @@
 public class MahasiswaMain11 {
-    
+    public static void main(String[] args) {
+        // Membuat objek MataKuliah
+        MataKuliah11 mk1 = new MataKuliah11();
+        mk1.namaMatakuliah = "Algoritma dan Pemrograman";
+        mk1.kodeMatakuliah = "MK001";
+        mk1.sks = 3;
+
+        MataKuliah11 mk2 = new MataKuliah11();
+        mk2.namaMatakuliah = "Basis Data";
+        mk2.kodeMatakuliah = "MK002";
+        mk2.sks = 3;
+
+        MataKuliah11 mk3 = new MataKuliah11();
+        mk3.namaMatakuliah = "Matematika Diskrit";
+        mk3.kodeMatakuliah = "MK003";
+        mk3.sks = 2;
+
+        // Membuat objek Mahasiswa
+        Mahasiswa11 mahasiswa = new Mahasiswa11("2341720001", "Budi Santoso", 2023);
+
+        // Menambahkan mata kuliah
+        mahasiswa.tambahMataKuliah(mk1);
+        mahasiswa.tambahMataKuliah(mk2);
+        mahasiswa.tambahMataKuliah(mk3);
+
+        // Menampilkan informasi mahasiswa
+        System.out.println("\n=== Informasi Mahasiswa ===");
+        mahasiswa.tampilkanInfo();
+
+        // Menampilkan total SKS
+        System.out.println("\nTotal SKS yang diambil: " + mahasiswa.hitungTotalSKS());
+    }
 }
